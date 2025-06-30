617dfe8393807d40a42fb798880159f6d0395e58
YeungJC <124306701+YeungJC@users.noreply.github.com>
2025-06-30 17:30:45 +0100

    setting up env

---
+++ main.lua
@@ -0,0 +1,35 @@
+Object = require 'libraries/classic'
+
+function love.load()
+    local object_files = {}
+    recursiveEnumerate('objects', object_files)
+    requireFiles(object_files)
+
+end
+
+function recursiveEnumerate(folder,file_list)
+    local items = love.filesystem.getDirectoryItems(folder)
+    for _, item in ipairs(items) do 
+        local file = folder .. "/" .. item
+        if love.filesystem.isFile(file) then 
+            table.insert(file_list,file)
+        elseif love.filesystem.isDirectory(file) then 
+            recursiveEnumerate(file, file_list)
+        end
+    end
+end
+
+function requireFiles(files)
+    for _, file in ipairs(files) do 
+        local file = file:sub(1,-5)
+        require(file)
+    end
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.draw(image, 0, 0)
+end
